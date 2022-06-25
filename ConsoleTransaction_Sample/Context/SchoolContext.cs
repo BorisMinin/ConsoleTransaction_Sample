f5c400ed20880e4c3055ded875aac347bd3b2ced
BorisMinin <boris.minin@mail.ru>
2022-06-25 13:19:01 +0300

    Add entities and DbContext

---
+++ ConsoleTransaction_Sample/Context/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleTransaction_Sample.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleTransaction_Sample.Context
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ ConsoleTransaction_Sample/Entities/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleTransaction_Sample.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<Student> Students { get; set; } = new();
+    }
+}
